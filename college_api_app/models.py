f2fb01c1a896de7caa25cd953d4ddd7a3ac8e86e
dizinnes <dizinnes228@gmail.com>
2021-06-07 23:23:21 +0600

    feat(api/models): Create carcass for models

---
+++ college_api_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Country(models.Model):
+    pass
+
+
+class City(models.Model):
+    pass
+
+
+class Category(models.Model):
+    # Имеется ввиду государственный или частный
+    pass
+
+
+class College(models.Model):
+    pass
