c631a84b77e8a351d8c4d9409556b134ce972f87
dizinnes <dizinnes228@gmail.com>
2021-06-09 15:19:36 +0600

    feat(college_app/admin): Register model in admin panel

--- college_api_app/admin.py
+++ college_api_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from college_api_app.models import College, Category, City, Country
+
+
+@admin.register(College)
+class AdminCollege(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryCollege(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityCollege(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryCollege(admin.ModelAdmin):
+    pass
--- college_api_app/apps.py
+++ college_api_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CollegeApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'college_api_app'
+    verbose_name = 'College api'
