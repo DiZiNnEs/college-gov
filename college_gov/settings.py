ce5a3474f89af5127f71b6cffac3a6e800d697b2
dizinnes <dizinnes228@gmail.com>
2021-06-10 18:56:42 +0600

    feat(project/settings): Add `REST_FRAMEWORK` constant to settings.py

--- college_gov/settings.py
+++ college_gov/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'college_api_app.apps.CollegeApiConfig',
 
+    'django_filters',
     'rest_framework',
 ]
 
@@ -127,3 +128,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
