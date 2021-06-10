3ed7c0c9b1cc7275497acf6662f6139a9aa2c2d0
dizinnes <dizinnes228@gmail.com>
2021-06-10 19:58:47 +0600

    feat(college_app/views): Add fields for filter, search and ordering

--- college_api_app/views.py
+++ college_api_app/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from college_api_app.models import College
 from college_api_app.serializers import CollegeSerializer
@@ -7,3 +9,7 @@ from college_api_app.serializers import CollegeSerializer
 class CollegeViewSet(viewsets.ModelViewSet):
     queryset = College.objects.all()
     serializer_class = CollegeSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ('category', 'type')
+    search_fields = ('title', 'country__name', 'city__name',)
+    ordering_fields = ('title',)
