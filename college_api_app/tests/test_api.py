3451ee7d8f999b2578f329ca7d2b9aa61d162d49
dizinnes <dizinnes228@gmail.com>
2021-06-11 20:15:35 +0600

    refactor(college_app/tests): Create method `setUp` and add create data to in

--- college_api_app/tests/test_api.py
+++ college_api_app/tests/test_api.py
@@ -13,25 +13,29 @@ from college_api_app.serializers import CollegeSerializer
 
 
 class CollegesApiTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.category_1 = Category.objects.create(name='Tech')
+        self.category_2 = Category.objects.create(name='IT')
+
+        self.country_1 = Country.objects.create(name='Kazakhstan', code='+77')
+        self.country_2 = Country.objects.create(name='Russian', code='+99')
+
+        self.city_1 = City.objects.create(name='Kokshetau')
+        self.city_2 = City.objects.create(name='Moscow')
+
+        self.college_1 = College.objects.create(title='KAZGTK', category=self.category_1, street='Pushkina 3',
+                                                email='kazgtk@gmail.com', type='PRV', country=self.country_1,
+                                                city=self.city_1,
+                                                budget=10000.231)
+        self.college_2 = College.objects.create(title='VTK', category=self.category_2, street='Tajenova 88',
+                                                email='VTK@mail.ru',
+                                                type='NAT', country=self.country_2, city=self.city_2,
+                                                budget=13213.231233)
+
     def test_get(self):
-        category_1 = Category.objects.create(name='Tech')
-        category_2 = Category.objects.create(name='IT')
-
-        country_1 = Country.objects.create(name='Kazakhstan', code='+77')
-        country_2 = Country.objects.create(name='Russian', code='+99')
-
-        city_1 = City.objects.create(name='Kokshetau')
-        city_2 = City.objects.create(name='Moscow')
-
-        college_1 = College.objects.create(title='KAZGTK', category=category_1, street='Pushkina 3',
-                                           email='kazgtk@gmail.com', type='PRV', country=country_1, city=city_1,
-                                           budget=10000.231)
-        college_2 = College.objects.create(title='VTK', category=category_2, street='Tajenova 88', email='VTK@mail.ru',
-                                           type='NAT', country=country_2, city=city_2, budget=13213.231233)
-
         url = reverse('api:college-list')
         response = self.client.get(url)
-        serializer_data = CollegeSerializer([college_1, college_2], many=True).data
+        serializer_data = CollegeSerializer([self.college_1, self.college_2], many=True).data
         self.assertEqual(serializer_data, response.data)
 
     def test_http_status_college_list(self):
--- college_api_app/tests/test_serializers.py
+++ college_api_app/tests/test_serializers.py
@@ -10,46 +10,49 @@ from college_api_app.serializers import CollegeSerializer
 
 
 class SerializersTestCase(TestCase):
-    def test_college_serializers(self):
-        category_1 = Category.objects.create(name='Tech')
-        category_2 = Category.objects.create(name='IT')
+    def setUp(cls):
+        cls.category_1 = Category.objects.create(name='Tech')
+        cls.category_2 = Category.objects.create(name='IT')
 
-        country_1 = Country.objects.create(name='Kazakhstan', code='+77')
-        country_2 = Country.objects.create(name='Russian', code='+99')
+        cls.country_1 = Country.objects.create(name='Kazakhstan', code='+77')
+        cls.country_2 = Country.objects.create(name='Russian', code='+99')
 
-        city_1 = City.objects.create(name='Kokshetau')
-        city_2 = City.objects.create(name='Moscow')
+        cls.city_1 = City.objects.create(name='Kokshetau')
+        cls.city_2 = City.objects.create(name='Moscow')
 
-        college_1 = College.objects.create(title='KAZGTK', category=category_1, street='Pushkina 3',
-                                           email='kazgtk@gmail.com', type='PRV', country=country_1, city=city_1,
+        cls.college_1 = College.objects.create(title='KAZGTK', category=cls.category_1, street='Pushkina 3',
+                                           email='kazgtk@gmail.com', type='PRV', country=cls.country_1, city=cls.city_1,
                                            budget=1000.02302)
-        college_2 = College.objects.create(title='VTK', category=category_2, street='Tajenova 88', email='VTK@mail.ru',
-                                           type='NAT', country=country_2, city=city_2, budget=3545323.123493)
-        data = CollegeSerializer([college_1, college_2], many=True).data
-        college_1_data = CollegeSerializer(college_1, many=False).data
-        college_2_data = CollegeSerializer(college_2, many=False).data
+        cls.college_2 = College.objects.create(title='VTK', category=cls.category_2, street='Tajenova 88', email='VTK@mail.ru',
+                                           type='NAT', country=cls.country_2, city=cls.city_2, budget=3545323.123493)
+        data = CollegeSerializer([cls.college_1, cls.college_2], many=True).data
+        cls.college_1_data = CollegeSerializer(cls.college_1, many=False).data
+        cls.college_2_data = CollegeSerializer(cls.college_2, many=False).data
+
+    def test_college_serializers(self):
+
         expected_data_college_1 = {
-            'id': college_1.id,
+            'id': self.college_1.id,
             'type': 'PRV',
             'title': 'KAZGTK',
-            'category': category_1.pk,
+            'category': self.category_1.pk,
             'street': 'Pushkina 3',
             'email': 'kazgtk@gmail.com',
-            'country': country_1.id,
-            'city': city_1.pk,
+            'country': self.country_1.id,
+            'city': self.city_1.pk,
             'budget': '1000.0230200000',
         }
         expected_data_college_2 = {
-            'id': college_2.id,
+            'id': self.college_2.id,
             'type': 'NAT',
             'title': 'VTK',
-            'category': category_2.pk,
+            'category': self.category_2.pk,
             'street': 'Tajenova 88',
             'email': 'VTK@mail.ru',
-            'country': country_2.id,
-            'city': city_2.pk,
+            'country': self.country_2.id,
+            'city': self.city_2.pk,
             'budget': '3545323.1234930000',
         }
 
-        self.assertEqual(expected_data_college_1, college_1_data)
-        self.assertEqual(expected_data_college_2, college_2_data)
+        self.assertEqual(expected_data_college_1, self.college_1_data)
+        self.assertEqual(expected_data_college_2, self.college_2_data)
